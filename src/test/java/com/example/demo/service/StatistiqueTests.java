d79fd08abfa2131c36143908d5667d4e1e2e0a13
BoranCAV <cav.boran@gmail.com>
2026-06-17 14:58:57 +0200

    TP2: tests JUnit de StatistiqueImpl (prix moyen + exception)

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,20 +5,43 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class StatistiqueTests {
 
     @Test
-    void testStatistique() {
+    void prixMoyenDeDeuxVoituresIdentiques() {
         StatistiqueImpl statistique = new StatistiqueImpl();
-        Voiture v1 = new Voiture("Ferrari", 3000);
-        Voiture v2 = new Voiture("Porsche", 3000);
-        statistique.ajouter(v1);
-        statistique.ajouter(v2);
+        statistique.ajouter(new Voiture("Ferrari", 3000));
+        statistique.ajouter(new Voiture("Porsche", 3000));
         Echantillon echantillon = statistique.prixMoyen();
         assertEquals(3000, echantillon.getPrixMoyen());
         assertEquals(2, echantillon.getNombreDeVoitures());
     }
 
+    @Test
+    void prixMoyenDeDeuxVoituresDifferentes() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
+        statistique.ajouter(new Voiture("Ferrari", 1000));
+        statistique.ajouter(new Voiture("Porsche", 3000));
+        Echantillon echantillon = statistique.prixMoyen();
+        assertEquals(2000, echantillon.getPrixMoyen());
+        assertEquals(2, echantillon.getNombreDeVoitures());
+    }
+
+    @Test
+    void prixMoyenAvecUneSeuleVoiture() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
+        statistique.ajouter(new Voiture("Toyota", 1500));
+        Echantillon echantillon = statistique.prixMoyen();
+        assertEquals(1500, echantillon.getPrixMoyen());
+        assertEquals(1, echantillon.getNombreDeVoitures());
+    }
+
+    @Test
+    void prixMoyenSansVoitureLeveUneException() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
+        assertThrows(ArithmeticException.class, () -> statistique.prixMoyen());
+    }
 }
